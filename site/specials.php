02429f2651e7587dd06862053ae96c6532022e91
Saabbass <178414@novacollege.nl>
2023-04-02 18:26:48 +0200

    17

--- site/specials.php
+++ site/specials.php
@@ -62,8 +62,9 @@ $recepten_maxingredients = mysqli_fetch_all($result, MYSQLI_ASSOC);
                         <?php endforeach;?>
                     </section>
                 </div>
-                
+
                 <h2 class="recept_orderd">De makkelijkste recepten</h2>
+                
                 <div class="recept_container">
                     <section class="cards_container">
                         <?php foreach($recepten_easy as $reasy):?>
